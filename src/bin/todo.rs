897c7b0e1c731adad674b08fce0c5ed89ddd0e6f
Graham Home <grahamhome333@gmail.com>
2023-03-06 11:38:34 -0500

    Refactor to handle database errors within database functions

--- src/bin/todo.rs
+++ src/bin/todo.rs
@@ -1,7 +1,6 @@
 extern crate core;
 
 use std::env;
-use diesel::result::{DatabaseErrorKind, Error};
 use my_todo::db::*;
 
 /// A CLI tool for interacting with the to-do app database.
@@ -16,6 +15,7 @@ fn help() {
     println!("\tdelete <title>: Delete task with title");
 }
 
+/// CLI entry point: calls subcommand functions based on user input.
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -27,12 +27,13 @@ fn main() {
         "help" => help(),
         "new" => new_task(&args[2..]),
         "show" => show_tasks(&args[2..]),
-        "finish" => mark_task_done(&args[2..]),
+        "finish" => finish(&args[2..]),
         "delete" => delete(&args[2..]),
         _ => help(),
     }
 }
 
+/// Calls database function to create new task and handles errors.
 fn new_task(args: &[String]) {
     if args.len() != 1 {
         println!("new: expects one argument, got {}", args.len());
@@ -42,15 +43,11 @@ fn new_task(args: &[String]) {
     let conn = establish_connection();
     match create_task(&conn, &args[0]) {
         Ok(_) => (),
-        Err(err) => match err {
-            Error::DatabaseError(DatabaseErrorKind::UniqueViolation, err) => {
-                println!("new: a task with title '{}' already exists", &args[0])
-            },
-            _ => println!("An error occurred while attempting to create the task")
-        }
-    };
+        Err(msg) => println!("{}", msg),
+    }
 }
 
+/// Calls database function to list all tasks.
 fn show_tasks(args: &[String]) {
     if args.len() > 0 {
         println!("show: unexpected argument");
@@ -71,7 +68,8 @@ fn show_tasks(args: &[String]) {
     }
 }
 
-fn mark_task_done(args: &[String]) {
+/// Calls database function to update task status. Handles any errors.
+fn finish(args: &[String]) {
     if args.len() != 2 {
         println!("finish: expects two arguments, got {}", args.len());
         help();
@@ -85,13 +83,13 @@ fn mark_task_done(args: &[String]) {
         } else { panic!("finish: second argument must be 0 or 1")}
     };
     let conn = establish_connection();
-    let task_query_result = query_tasks_by_title(&conn, &args[0]).pop();
-    match task_query_result {
-        Some(task) => set_task_done_status(&conn, task.id, done_status),
-        None => println!("No task has title '{}'", &args[0]),
+    match query_tasks_by_title(&conn, &args[0]) {
+        Ok(task) => set_task_done_status(&conn, task.id, done_status),
+        Err(msg) => println!("{}", msg),
     };
 }
 
+/// Calls database function to delete a task, handling any errors.
 fn delete(args: &[String]) {
     if args.len() < 1 {
         println!("delete: expects one argument, got {}", args.len());
@@ -99,9 +97,8 @@ fn delete(args: &[String]) {
         return;
     };
     let conn = establish_connection();
-    let task_query_result = query_tasks_by_title(&conn, &args[0]).pop();
-    match task_query_result {
-        Some(task) => delete_task(&conn, task.id),
-        None => println!("No task has title '{}'", &args[0]),
+    match query_tasks_by_title(&conn, &args[0]) {
+        Ok(task) => delete_task(&conn, task.id),
+        Err(msg) => println!("{}", msg),
     };
 }
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,40 +1,64 @@
 use diesel::{prelude::*, sqlite::SqliteConnection};
+use diesel::result::{DatabaseErrorKind, Error};
 
 pub mod models;
 pub mod schema;
 
+/// Database functions used throughout the application.
+
+/// Creates and returns a database connection.
 pub fn establish_connection() -> SqliteConnection {
     let db = "./tododb.sqlite3";
     SqliteConnection::establish(db)
         .unwrap_or_else(|_| panic!("Error connecting to {}", db))
 }
 
-pub fn create_task(connection: &SqliteConnection, title: &str) -> QueryResult<usize> {
+/// Inserts a new task into the database.
+pub fn create_task(connection: &SqliteConnection, title: &str) -> Result<(), String> {
     let task = models::NewTask { title, done: 0 };
 
-    diesel::insert_into(schema::task::table)
+    match diesel::insert_into(schema::task::table)
         .values(&task)
-        .execute(connection)
+        .execute(connection) {
+        Ok(_) => Ok(()),
+        Err(err) => match err {
+            Error::DatabaseError(DatabaseErrorKind::UniqueViolation, _err) => {
+                Err(format!("new: a task with title '{}' already exists", title))
+            },
+            _ => Err(String::from("An error occurred while attempting to create the task"))
+        }
+    }
 }
 
+/// Loads all tasks from the database.
 pub fn list_tasks(connection: &SqliteConnection) -> Vec<models::Task> {
     schema::task::table
         .load::<models::Task>(connection)
         .expect("Error loading tasks")
 }
 
-pub fn query_tasks_by_title(connection: &SqliteConnection, title: &str) -> Vec<models::Task> {
-    schema::task::table.filter(schema::task::title.eq(title)).load::<models::Task>(connection).expect("Error filtering tasks by title")
+/// Queries database eturns a Result containing either a task with the given title
+/// or None if no such task is found.
+pub fn query_tasks_by_title(connection: &SqliteConnection, title: &str) -> Result<models::Task, String> {
+    match schema::task::table.filter(schema::task::title.eq(title)).first::<models::Task>(connection) {
+        Ok(task) => Ok(task),
+        Err(e) => match e {
+            diesel::NotFound => Err(format!("No task has title '{title}'")),
+            _ => Err(String::from("Error querying tasks by title")),
+        }
+    }
 }
 
+/// Updates the status of the task with the given ID to the given value.
 pub fn set_task_done_status(connection: &SqliteConnection, task_id: i32, done_status: bool) {
     let done_status = match done_status {
         true => 1,
         false => 0,
     };
-    diesel::update(schema::task::table.find(task_id)).set(schema::task::done.eq(done_status)).execute(connection).expect(format!("Error updating task with id {}", task_id).as_str());
+    diesel::update(schema::task::table.find(task_id)).set(schema::task::done.eq(done_status)).execute(connection).expect(format!("Error updating task with id {task_id}").as_str());
 }
 
+/// Deletes the task with the given ID from the database.
 pub fn delete_task(connection: &SqliteConnection, task_id: i32) {
-    diesel::delete(schema::task::table.find(task_id)).execute(connection).expect(format!("Error deleting task with id {}", task_id).as_str());
+    diesel::delete(schema::task::table.find(task_id)).execute(connection).expect(format!("Error deleting task with id {task_id}").as_str());
 }
