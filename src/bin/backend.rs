603469cee956851361c738f45847ed2388b304ac
Graham Home <grahamhome333@gmail.com>
2023-03-07 19:10:01 -0500

    Serialize API responses into json

--- src/bin/backend.rs
+++ src/bin/backend.rs
@@ -3,16 +3,24 @@
 #[macro_use]
 extern crate rocket;
 
+#[macro_use]
+extern crate rocket_contrib;
+
+#[macro_use]
+extern crate serde;
+
 use my_todo::db::models::Task;
 use my_todo::db::{establish_connection, list_tasks};
+use rocket_contrib::json::Json;
+
+#[derive(Serialize)]
+struct JsonApiResponse {
+    data: Vec<Task>
+}
 
 #[get("/tasks")]
-fn tasks_get() -> String {
-    list_tasks(&establish_connection())
-        .into_iter()
-        .map(|task| task.title)
-        .collect::<Vec<String>>()
-        .join("\n")
+fn tasks_get() -> Json<JsonApiResponse> {
+    Json(JsonApiResponse{ data: list_tasks(&establish_connection()), })
 }
 
 fn main() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 #[macro_use]
 extern crate diesel;
 
+#[macro_use]
+extern crate serde;
+
 pub mod db;
+
+
